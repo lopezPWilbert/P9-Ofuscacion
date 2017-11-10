1829ee40a04f397c49103f963734bae3db06f4b2
Wilbert Lopez <wilbert.lopez17@tectijuana.edu.mx>
2017-11-09 22:06:58 -0800

    v1.1 Ofuscado.

--- P9 Ofuscacion/P9_Ofuscacion.py
+++ P9 Ofuscacion/P9_Ofuscacion.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*- 
 #Hora=28, si h>30, 3% mas por h extra
-
+'''
 while True:
     print "------------------------------------"
     horas=input("Horas trabajadas> ")
@@ -16,6 +16,27 @@ while True:
         print "Pago h. extras= $"+`(extras*28*1.03)`
         print "Pago total= $"+`pago`
 
-
-
+'''
+# -*- coding: utf-8 -*-
+#Hora=28, si h>30, 3% mas por h extra
+if 64 - 64: i11iIiiIii
+while True :
+ print "------------------------------------"
+ OO0o = input ( "Horas trabajadas> " )
+ if ( OO0o <= 30 ) :
+  Oo0Ooo = OO0o * 28
+  if 85 - 85: OOO0O0O0ooooo % IIii1I . II1 - O00ooooo00
+  print "Pago total= $" + ` Oo0Ooo `
+ else :
+  Oo0Ooo = OO0o * 28
+  I1IiiI = OO0o - 30
+  Oo0Ooo = Oo0Ooo + ( I1IiiI * 28 * 0.03 )
+  print "Horas extras: " + ` I1IiiI `
+  print "Pago h. extras= $" + ` ( I1IiiI * 28 * 1.03 ) `
+  print "Pago total= $" + ` Oo0Ooo `
+  if 27 - 27: iIiiiI1IiI1I1 * IIiIiII11i * IiIIi1I1Iiii - Ooo00oOo00o
+  if 48 - 48: oO0o / OOooOOo / I11i / Ii1I
+  if 48 - 48: iII111i % IiII + I1Ii111 / ooOoO0o * o00O0oo
+  if 97 - 97: oO0o0ooO0 - IIII / OOooOOo - oO0o0ooO0
+# dd678faae9ac167bc83abf78e5cb2f3f0688d3a3
 
